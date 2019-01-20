57c027976881cb2cb56435b4f2df4824f5020dc6
Giacomo Debidda <jackdebidda@gmail.com>
2019-01-20 15:58:31 +0100

    Add scaffolding for additional vx type definitions

---
+++ types/vx__demo/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for @vx/demo 0.0.183
+// Project: https://github.com/hshoff/vx
+// Definitions by: Giacomo Debidda <https://github.com/jackdbd>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "@vx/demo" {}
---
+++ types/vx__heatmap/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for @vx/heatmap 0.0.183
+// Project: https://github.com/hshoff/vx
+// Definitions by: Giacomo Debidda <https://github.com/jackdbd>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "@vx/heatmap" {}
