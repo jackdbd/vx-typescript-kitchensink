6c4795f278b46a69eb1e283357cad5360f4a170f
Giacomo Debidda <jackdebidda@gmail.com>
2019-02-10 21:22:29 +0100

    Add type definitions for vx/marker

--- types/vx__marker/index.d.ts
+++ types/vx__marker/index.d.ts
@@ -1,6 +1,31 @@
-// Type definitions for @vx/marker 0.0.183
+// Type definitions for @vx/marker 0.0.184
 // Project: https://github.com/hshoff/vx
 // Definitions by: Giacomo Debidda <https://github.com/jackdbd>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "@vx/marker" {}
+declare module "@vx/marker" {
+  import React from "react";
+
+  interface IProps {
+    className?: string;
+    from?: any;
+    label?: any; // string | function
+    labelAnchor?: string;
+    labelDx?: number;
+    labelDy?: number;
+    labelFill?: string;
+    labelFontSize?: number | string;
+    labelPaintOrder?: string;
+    labelStroke?: string;
+    labelStrokeWidth?: number | string;
+    left?: number;
+    stroke?: string;
+    strokeWidth?: number;
+    strokeDasharray?: string;
+    to?: any;
+    top?: number;
+    transform?: string;
+  }
+
+  const Marker: React.ComponentType<IProps>;
+}
